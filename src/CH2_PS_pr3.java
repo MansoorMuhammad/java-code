d3b962a02a35ffb76b91626849378afddc628fc5
MansoorMuhammad <muhammadokashag@gmail.com>
2025-07-18 13:20:34 -0700

    CH2 PS PR$

---
+++ src/CH2_PS_pr3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CH2_PS_pr3 {
+    public static void main(String[] args) {
+
+        Scanner forInput = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int a = forInput.nextInt();
+        System.out.println(a>8);
+
+    }
+}
