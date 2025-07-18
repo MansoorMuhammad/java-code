43e3a08a16b7456dc01260ffaf1dbf8dc5ff9a66
MansoorMuhammad <muhammadokashag@gmail.com>
2025-07-18 13:11:35 -0700

    literals

---
+++ src/chp02Literlas.java
@@ -0,0 +1,18 @@
+public class chp02Literlas {
+
+    public static void main(String[] args) {
+        int age = 12;
+        byte age2= 23;
+        char ch= 'M';
+        double num=123456d;
+        double num2=89.5678d;
+        float number =456789f;
+
+        System.out.println(" integer: " + age);
+        System.out.println(" Byte : " + age2);
+        System.out.println(" character: " + ch);
+        System.out.println(" Double 1: " + num);
+        System.out.println(" Double 2:" + num2);
+        System.out.println(" Float: "+ number);
+    }
+}
