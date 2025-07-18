34ed01ad94cae86b97fc62e68730b41c7c898de8
MansoorMuhammad <muhammadokashag@gmail.com>
2025-07-18 13:13:37 -0700

    chp 1 ps

---
+++ src/CH1_PS.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+public class CH1_PS {
+    public static void main(String[] args) {
+        Scanner forUser = new Scanner(System.in);
+//        System.out.println("Enter Three Numbers: ");
+//        int a = forUser.nextInt();
+//        int b = forUser.nextInt();
+//        int c = forUser.nextInt();
+////        int a = 10;
+////        int b = 10;
+////        int c = 10;
+//        float sum;
+//        sum = a + b + c;
+//        System.out.println("sum is : " + sum);
+
+        //program for calculation of cgpa
+//        float subject1 = 87.89f;
+//        int subject2 = 57;
+//        int subject3 = 66;
+//        double cgpa;
+//        cgpa = (subject1 + subject2 + subject3)/300;
+//        System.out.println("Your Cgpa is: " + cgpa);
+
+        //program for taking data from the user in the form of his name and telling him have a good day
+        System.out.print("Enter Your Name: ");
+        String a =  forUser.nextLine();
+        System.out.println(" Hello! " + a + " have a good day. ");
+
+
+
+
+    }
+}
