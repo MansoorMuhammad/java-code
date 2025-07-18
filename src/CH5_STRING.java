76795fa01c14817c1bf64abc8bcdeda92b70a005
MansoorMuhammad <muhammadokashag@gmail.com>
2025-07-18 13:21:11 -0700

    String

---
+++ src/CH5_STRING.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CH5_STRING {
+    public static void main(String[] args) {
+//        String name = new String("Mansoor Muhammad" );
+//        System.out.println(name);
+//        Scanner forInp = new Scanner(System.in);
+//        System.out.println("Enter Your Name: ");
+//        String a = forInp.next();//just for printing one word
+//          String a = forInp.nextLine();// for printing the whole sentence
+
+//        System.out.println("Answer: " + a);
+
+        //program for system.out.printf();
+
+        int a = 6;
+        float b = 6.567f;
+//        System.out.format("The value of a is %d & the value of b is %f ", a , b);
+        System.out.printf("The value of a is %d & the value of b is %f ", a , b);
+        // %d && %f are known as FORMAT SPECIFIER// %d && %f are known as FORMAT SPECIFIER
+        // %d represents for integer data-type while %f represents float data-type
+        //System.out.printf && System.out.format are the same thing
+    }
+}
