d353bacdac7b22ff95fbc36f0d132b323ad30dd0
MansoorMuhammad <muhammadokashag@gmail.com>
2025-07-23 12:38:44 -0700

    CH06_ps

---
+++ src/CH5_StringMethods.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+
+public class CH5_StringMethods {
+    public static void main(String[] args) {
+//        String name = "           Mansoor Muhammad FUUAST ";
+//        int value = name.length();
+//        String lower = name.toLowerCase();
+//        String upper = name.toUpperCase();
+        String nonTrim = "     Mansoor Muhammad FUUASTIAN ";
+        String forTrim = nonTrim.trim();
+//        System.out.println("NUmber of letters are: " + value);
+//        System.out.println("To Lower Case: " + lower);
+//        System.out.println(upper);
+        System.out.println("Non-trim Value: " + nonTrim);
+        System.out.println("Trim value : " + forTrim);
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+package company.com;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        System.out.println("Mansoor Muhammad");
+        System.out.println("Why not");
+        System.out.println("Loop Practice");
+        System.out.println("I dont believe that i am doing JAVA");
+        String name="Imad Yousafzai";
+        System.out.println("Your Name is: " + name);
+        String define = "Gul Zareen Khan";
+        System.out.println("Your Father Name is: " + define);
+        int  roll_nmbr = 1234;
+        System.out.println("Your Roll Number is: " + roll_nmbr);
+        String sem="First";
+        System.out.println("Semister Is: " + sem);
+        char Sec='C';
+        System.out.println("Semister is: "+ Sec);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Hello, World!");
+//        }
+//        for(int j=1; j<=10; j++){
+//            System.out.println("Why i am here");
+//        }
+
+
+    }
+}
